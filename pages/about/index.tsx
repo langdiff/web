803ff013b5c6fc184924cd1b6cc4b7eac646341b
Danny Allegrezza <danny@dannyallegrezza.com>
2021-11-18 23:32:04 -0500

    testing out <Head> component

---
+++ pages/about/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>About us!</title>
+      </Head>
+      <div>About us</div>
+    </div>
+  );
+};
+
+export default About;
